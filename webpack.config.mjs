378a290a3efe6195ca1e8cc71e35ffb121d1de05
Viktar <natgeogr93@gmail.com>
2024-10-19 10:25:04 +0300

    initial setup

---
+++ webpack.config.mjs
@@ -0,0 +1,27 @@
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "production",
+  target: "node",
+};
